84cdd2b50a606673aa5c520fa99c12798c77de55
Fartash Haghani <fartashh@gmail.com>
2026-05-11 13:14:06 -0400

    Add GitHub Actions deployment workflow and configure dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Dynamically set the base path if building within GitHub Actions
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   plugins: [react()],
 })
